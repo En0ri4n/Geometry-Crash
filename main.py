e8cd510cb5084b4aa242c2502226f6a2a26fac00
Enorian <enorian.rajo@gmail.com>
2023-05-09 19:04:14 +0200

    update all (wrong file)

--- main.py
+++ main.py
@@ -1,87 +1,141 @@
-def main():
-    str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    list = []
-    for i in range(0, 26):
-        list.append(str[i])
+import pygame
+from pygame.event import Event
+from pygame.locals import *
 
-    print(list)
+BLOCK_SIZE = 100
 
+vec = pygame.math.Vector2
 
-if __name__ == '__main__':
-    main()
+ACC = 0.5
+FRIC = -0.12
 
+# Define the screen size (width and height)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-def decode():
-    key = input("Insert Word Key :").upper()
-    coded_word = input("Insert Coded Word :").upper()
+FPS = 60
+FramePerSec = pygame.time.Clock()
 
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-                'V', 'W', 'X', 'Y', 'Z']
-
-    z = []
-    for i in range(0, len(coded_word)):
-        z.append(alphabet.index(coded_word[i]))
-
-    y = []
-    key_index = 0
-    for i in range(0, len(coded_word)):
-        y.append(alphabet.index(key[key_index]))
-        if key_index + 1 >= len(key):
-            key_index = 0
-        else:
-            key_index += 1
-
-    decoded_word = ""
-    letter_index = 0
-    for i in range(0, len(coded_word)):
-        letter_index = z[i] - y[i]
-        decoded_word += alphabet[letter_index]
-
-    print("Y : " + str(y))
-    print("Z : " + str(z))
-    print("Decoded Word : " + decoded_word)
-
-    decoded_map = []
-    for i in range(0, len(decoded_word)):
-        decoded_map.append(decoded_word[i] + "=" + str(alphabet.index(decoded_word[i])))
-
-    print("Decoded Map (x) : " + str(decoded_map))
+MAP = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1]]
 
 
-def encode():
-    key = input("Insert Word Key :").upper()
-    uncoded_word = input("Insert Word To Encode :").upper()
+class GeometryCrash:
 
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-                'V', 'W', 'X', 'Y', 'Z']
+    def __init__(self):
 
-    x = []
-    for i in range(0, len(uncoded_word)):
-        x.append(alphabet.index(uncoded_word[i]))
+        pygame.init()  # Init PyGame
 
-    y = []
-    key_index = 0
-    for i in range(0, len(uncoded_word)):
-        y.append(alphabet.index(key[key_index]))
-        if key_index + 1 >= len(key):
-            key_index = 0
-        else:
-            key_index += 1
+        # Set up the window
+        self.screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+        pygame.display.set_caption("Geometry Crash - by En0ri4n")
 
-    encoded_word = ""
-    letter_index = 0
-    for i in range(0, len(uncoded_word)):
-        letter_index = x[i] + y[i]
-        while letter_index >= len(alphabet):
-            letter_index -= 26
-        encoded_word += str(alphabet[letter_index])
+        # Set up map view
+        self.map_x = 0
 
-    print("X : " + str(x))
-    print("Y : " + str(y))
-    print("Encoded Word : " + encoded_word)
+        # Set up the player
+        self.player = Player(self)
 
-    encoded_map = []
-    for i in range(0, len(encoded_word)):
-        encoded_map.append(encoded_word[i] + "=" + str(alphabet.index(encoded_word[i])))
+        self.platforms = pygame.sprite.Group()
+        self.platforms.add(Tile())
 
-    print("Decoded Map (z) : " + str(encoded_map))
+        self.running = True
+
+        while self.running:
+            self.loop()
+
+        # If not running, quit all
+        pygame.quit()
+
+    def loop(self):
+        for current_event in pygame.event.get():
+            self.on_event(current_event)  # Listen for all events
+
+        self.player.move()
+
+        self.update()
+
+        self.render()
+
+        pygame.display.update()
+
+        FramePerSec.tick(FPS)
+
+    def update(self):
+        platforms_hit = pygame.sprite.spritecollide(self.player, self.platforms, False)
+        if platforms_hit:
+            self.player.pos.y = platforms_hit[0].rect.top + 1
+            self.player.vel.y = 0
+
+        self.map_x = 0
+
+    def render(self):
+
+        # Background (white)
+        self.screen.fill((255, 255, 255))
+
+        self.screen.blit(self.player.surf, self.player.rect)
+
+        for platform in self.platforms:
+            self.screen.blit(platform.surf, platform.rect)
+
+        # Draw time !
+        # pygame.draw.polygon(self.screen, (0, 0, 255), ((10, 10), (10, 20), (20, 20), (20, 10)))
+
+        # Update screen
+        pygame.display.flip()
+
+    def on_event(self, current_event: Event):
+
+        if current_event.type == KEYDOWN:  # When the user hit a key
+            if current_event.key == K_ESCAPE:  # If this is the escape key, stop the loop
+                self.running = False
+
+            self.player.on_key(current_event.key)
+
+        if current_event.type == pygame.QUIT:
+            self.running = False
+        pass
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, game: GeometryCrash):
+        super().__init__()
+        self.game = game
+        self.surf = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
+        self.surf.fill((128, 200, 200))
+        self.rect = self.surf.get_rect()
+
+        self.pos = vec((SCREEN_WIDTH / 2, 50))
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+
+    def move(self):
+        self.acc = vec(0, 0.5)
+
+        self.vel += self.acc
+        self.pos += self.vel + 0.8 * self.acc
+        self.rect.midbottom = self.pos
+
+    def on_key(self, key):
+        if key == K_SPACE:
+            self.jump()
+        pass
+
+    def jump(self):
+        self.vel.y = -12
+        pass
+
+
+class Tile(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
+        self.surf.fill((255, 0, 0))
+        self.surf.fill((100, 0, 0), (3, 3, BLOCK_SIZE - 6, BLOCK_SIZE - 6))
+        self.rect = self.surf.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - BLOCK_SIZE / 2))
+
+
+GeometryCrash()
